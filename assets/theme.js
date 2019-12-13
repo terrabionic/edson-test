05ea1d41506dde22bd9325e6983b1e623ab7864a
EdsonSoni <edson.soni@terrabionic.com.mx>
2019-12-13 12:02:35 -0600

    correcciones a slider de product desktop (incompleto)

--- assets/theme.js
+++ assets/theme.js
@@ -5190,3 +5190,20 @@ $(".js-carousel").each(function(){
 	$carouselButton.on("click", slide);
 });
 
+//carrousel vertical
+$('.carousel .vertical .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+  
+  for (var i=1;i<2;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
